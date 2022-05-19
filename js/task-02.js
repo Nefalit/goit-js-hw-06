fd81682082f1af1fdab65f612e8a26df0b868c98
Nefalit <nefalit7@gmail.com>
2022-05-19 18:21:58 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingridList = document.querySelector("#ingredients");
+
+const ingridItems = ingredients.map(el => {
+  const ingridItem = document.createElement("li");
+  ingridItem.classList.add("ingrid-item");
+  ingridItem.textContent = el;
+  return ingridItem;
+});
+console.log(ingridItems);
+
+ingridList.append(...ingridItems);
