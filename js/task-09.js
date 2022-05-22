f7efe68754a1584c5273156bfb4a326be9a874b1
Nefalit <nefalit7@gmail.com>
2022-05-22 13:25:40 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const bodyEl = document.querySelector("body")
+const colorChngBtnEl = document.querySelector(".change-color");
+const colorSpanEl = document.querySelector(".color")
+
+colorChngBtnEl.addEventListener("click", (ev) => {
+bodyEl.style.backgroundColor = getRandomHexColor();
+colorSpanEl.textContent = bodyEl.style.backgroundColor;
+})
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
